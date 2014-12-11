fcefceaa1c1d7519c44b1f5f7273736fc21a438d
Joe Ferrucci <mrferrucci@gmail.com>
2014-12-11 13:20:37 -0800

    seq search while-loop

--- Sorting/seqsearch.py
+++ Sorting/seqsearch.py
@@ -1,3 +1,15 @@
+def sequentialSearch(alist, item):
+	pos = 0
+	found = False
+
+	while pos < len(alist) and not found:
+		if alist[pos] == item:
+			found = True
+		else:
+			pos = pos + 1
+
+	return found
+
 testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-14	print(sequentialSearch(testlist, 3))
-15	print(sequentialSearch(testlist, 13))
+print(sequentialSearch(testlist, 3))
+print(sequentialSearch(testlist, 13))
